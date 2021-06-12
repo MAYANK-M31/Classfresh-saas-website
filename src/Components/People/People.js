3fa6a14c57b3a4c5ac2829d8f086cb47181b0042
MAYANK-M31 <mayank31m@gmail.com>
2021-06-13 00:36:42 +0530

    third commit

--- src/Components/People/People.js
+++ src/Components/People/People.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-
-const People =()=> {
-   
-        return (
-            <div>
-                people
-            </div>
-        );
-    
-}
-
-
+import React, { Component } from "react";
+import "../../css/People/People.css";
+import PeopleHeader from "./Header/PeopleHeader";
+const People = () => {
+  return (
+    <div className="Main-Div">
+      <PeopleHeader />
+      <div className="Inside-Div">
+        <div className="Left-Div"></div>
+        <div className="Middle-Div">
+          <div className="UpperBar-Div"></div>
+          <div className="Main-Inside-Div">
+            <div className="Container-Div"></div>
+            <div className="Right-Div"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default People;
