419f7941f08c2700deacf6b2f659bd2ffcbb1100
MAYANK-M31 <mayank31m@gmail.com>
2021-06-30 13:08:15 +0530

    fourthnew commit

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -3,6 +3,7 @@ import "../../css/Result/Result.css";
 import ResultHeader from "./Header/ResultHeader";
 import ResultSidebar from "./Sidebar/ResultSideBar";
 import ResultTable from "./TableView/ResultTable";
+import Split from "react-split";
 
 const Result = () => {
   const [addteacher, setaddteacher] = useState(false);
@@ -11,10 +12,22 @@ const Result = () => {
     <div className="Main-Div">
       <ResultHeader />
       <div className="Inside-Div">
-        <ResultSidebar />
-        <div className="Middle-Div">
-          <ResultTable/>
-        </div>
+        <Split
+          sizes={[15.5,84.5 ]}
+          minSize={0}
+          snapOffset={100}
+          gutterSize={5}
+          direction="horizontal"
+          cursor="col-resize"
+          className="split-flex" // You'll need to define this. check styles.css
+        >
+          {/* <div style={{width:"100%",backgroundColor:"red"}} > */}
+          <ResultSidebar />
+          {/* </div> */}
+          <div className="Middle-Div">
+            <ResultTable />
+          </div>
+        </Split>
       </div>
     </div>
   );
